d513ba59306d74f79347cd60ea52acc8b720ecbb
tools9090 <softwaresmarttool@gmail.com>
2023-07-25 21:45:49 +0530

    ee

--- core/settings.py
+++ core/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "9sd49d84fed48")                             #'django-insecure-*^0^nr2&6+=m8e6+ep%c*y+y)g*p0(x)k%+)fgli$$qopck^d%'
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
