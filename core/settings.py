4fdf53579e7dea378f2ca319287414017f5b14df
tools9090 <softwaresmarttool@gmail.com>
2023-07-25 21:05:06 +0530

    ss

--- core/settings.py
+++ core/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/app/static/'
 
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
